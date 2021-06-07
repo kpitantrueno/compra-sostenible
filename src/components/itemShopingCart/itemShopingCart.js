51bedfa605f9c401ab3e0a679ae45d7cde6964f9
ivan <ivanose93@gmail.com>
2021-06-07 13:42:20 +0100

    better display

--- src/components/itemShopingCart/itemShopingCart.js
+++ src/components/itemShopingCart/itemShopingCart.js
@@ -3,54 +3,68 @@ import './itemShopingCart.css';
 
 
 export default function ItemShopingCart(props) {
-   
-    
-    
+
+
+
     let json = JSON.parse(props.productData)
-    
 
 
-   function sum(){
 
-    props.addShopingCart({name:json.name,count:json.count+1})
+    function sum() {
 
-   }
-
-
-   function subt(){
-
-    if(json.count>0){
-
-    props.addShopingCart({name:json.name,count:json.count-1})
+        props.addShopingCart({ name: json.name, count: json.count + 1 })
 
     }
 
-   }
+
+    function subt() {
+
+        if (json.count > 0) {
+
+            props.addShopingCart({ name: json.name, count: json.count - 1 })
+
+        }
+
+    }
 
 
     return (
 
         <div className="card p-2 m-2" >
-            <div className ="card-body">
-            <h5 className ="card-title">{json.name}</h5>
-             <p>Cantidad: {json.count}</p>
-            <button className="btn btn-success" onClick={sum}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                </svg></button>
-            <button className="btn btn-primary" onClick={subt}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                </svg>
-            </button>
-            <button className="btn btn-danger" onClick={ e => {props.deleteItem(json.name)}} ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                </svg>
-            </button>
-            <picture className="position-absolute bottom-0 end-0">
-            <img src={json.img}></img>
-            </picture>
+
+            <div className="card-body">
+
+                <div className='container'>
+
+                    <div className='row'>
+
+                        <picture className="col-3">
+                            <img src={json.img}></img>
+                        </picture>
+
+                        <h5 className="card-title col-4">{json.name}</h5>
+                        <p className='col-1'>Cantidad: {json.count}</p>
+
+
+                        <button className="btn btn-success col-2" onClick={sum}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                        </svg></button>
+                        <button className="btn btn-primary col-1" onClick={subt}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                        </button>
+                        <button className="btn btn-danger col-1" onClick={e => { props.deleteItem(json.name) }} ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                        </svg>
+                        </button>
+
+
+
+                    </div>
+                </div>
 
             </div>
         </div>
--- src/components/shoopingCart/shoopingCart.js
+++ src/components/shoopingCart/shoopingCart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ItemShoopingCart from '../itemShopingCart/itemShopingCart'
 import { useState } from 'react'
+import './shopingCart.css';
 
 
 export default function ShoopingCart(props) {
@@ -12,21 +13,24 @@ export default function ShoopingCart(props) {
 
         <div className='container'>
 
-            <div className='container row'>
-                {
+         
 
-                    props.cosas.map(product => <div  className='col-4'><ItemShoopingCart productData={product} addShopingCart={props.purchase} deleteItem={props.deleteItem} /></div> )
+            <div className='shopingcart row '>
+                {
+                    
+                    props.cosas.map(product => <div  className='col-12 mt-5'><ItemShoopingCart productData={product} addShopingCart={props.purchase} deleteItem={props.deleteItem} /></div> )
                 }
 
+                
+
+
             </div>
 
-            <div className='container col-12 d-flex flex-row justify-content-evenly mb-4'>
+                    <button onClick={props.savePurchase} className='btn btn-secondary col-4'>Guardar compra</button>
+                    <button onClick={props.loadPurchase} className='btn btn-secondary col-4'>Cargar ultima compra</button>
+                    <button onClick={props.deleteAll} className='btn btn-secondary col-4'>Eliminar compra</button>
                 
-                    <button onClick={props.savePurchase} className='btn btn-secondary'>Guardar compra</button>
-                    <button onClick={props.loadPurchase} className='btn btn-secondary'>Cargar ultima compra</button>
-                    <button onClick={props.deleteAll} className='btn btn-secondary'>Eliminar compra</button>
-                
-            </div>
+           
             
         </div>
 
